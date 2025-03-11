e6d9457ac7add37397f307bd00ab888d87e8af4f
Shivam Goel <24bme120@nith.ac.in>
2025-03-11 14:36:03 +0530

    added SponsorTracks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import About from '@/components/About';
 import Landing from '@/components/Landing';
+import SponsorTracks from '@/components/sponsorTracks';
 import React from 'react'
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main>
       <Landing />
       <About/>
+      <SponsorTracks/>
     </main>
   );
 }
