1f706677a5e3cb1be5079eb8c09bc1185287cd2a
frenzywho <amvermagaurav007@gmail.com>
2025-03-21 13:47:09 +0530

    Resolved merge conflict in src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import About from '@/components/About';
 import Landing from '@/components/Landing';
-import SponsorsSection from '@/components/SponsorsSection';
+import SponsorTracks from '@/components/sponsorTracks';
 import PrizePool from '@/components/PrizePool';
 
 
-import SponsorTracks from '@/components/sponsorTracks';
 export default function Home() {
   return (
     <main>
       <Landing />
       <About/>
       <PrizePool/>
-      <SponsorsSection/>
       <SponsorTracks/>
     </main>
   );
